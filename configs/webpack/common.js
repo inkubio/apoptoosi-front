f2237b4cbb9e9024a03032c71527cd7a4aa59b31
vahvero <otso.brummer@gmail.com>
2018-10-23 14:32:23 +0300

    Added correct front

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -34,6 +34,10 @@ module.exports = {
         ],
       },
       {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=public/fonts/[name].[ext]'
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
