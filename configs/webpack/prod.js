4cd667a638c1688440dcdbd98d3133a42cec298e
vahvero <otso.brummer@gmail.com>
2018-11-08 16:07:53 +0200

    CHanged production default route to /inkubio15/. Developer should change this back to further development

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -10,7 +10,7 @@ module.exports = merge(commonConfig, {
   output: {
     filename: 'js/bundle.[hash].min.js',
     path: resolve(__dirname, '../../dist'),
-    publicPath: '/',
+    publicPath: '/inkubio15/',
   },
   devtool: 'source-map',
   plugins: [],
