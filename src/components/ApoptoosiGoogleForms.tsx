3a0c8d9890ae6025e7ae749c86cb46aa231bf160
vahvero <otso.brummer@gmail.com>
2018-11-15 17:22:45 +0200

    Some css to it. Still ugly

--- src/components/ApoptoosiGoogleForms.tsx
+++ src/components/ApoptoosiGoogleForms.tsx
@@ -26,30 +26,43 @@ export const ApoptoosiGoogleForms: React.SFC<IApoptoosiGoogleFormsProps> = ({url
         <form className="form" action={url + '/formResponse'} target="noLooktarget" id="kollariform">
             {/* <p>{url}</p> */}
             <fieldset>
-            <ul>
-                <li>
-                    <label>Nimi</label>
-                    <input name="entry.700536277" type="text" required />
-                </li>
-                <li>
-                    <label>Sähköposti</label>
-                    <input name="entry.1543381985" type="email" required />
-                </li>
-                <li>
-                    <label>Koko</label>
-                    <select name="entry.1260883230">
-                        <option value="XS">XS</option>
-                        <option value="S">S</option>
-                        <option value="M">M</option>
-                        <option value="L">L</option>
-                        <option value="XL">XL</option>
-                        <option value="XXL">XXL</option> 
-                    </select>
-                </li>
+                <legend>Paita tilaus</legend>
+            <table>
+                <tr>
+                    <td>
+                        <label>Nimi</label>
+                    </td>
+                    <td>
+                        <input name="entry.700536277" type="text" required />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Sähköposti</label>
+                    </td>
+                    <td>
+                        <input name="entry.1543381985" type="email" required />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Koko</label>
+                    </td>
+                    <td>
+                        <select name="entry.1260883230">
+                            <option value="XS">XS</option>
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                            <option value="XXL">XXL</option> 
+                        </select>
+                    </td>
+                </tr>
                 <li>
                     <input type="submit" value="Lähetä" />
                 </li>
-            </ul>
+            </table>
             </fieldset>
         </form>
         </>
